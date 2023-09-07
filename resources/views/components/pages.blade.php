4edbb4098854629f3cb757a355effff7192d3b8e
Cuji12 <cu.janeway@gmail.com>
2023-09-07 12:29:26 +0100

    - Make whole list item area clickable.

--- resources/views/components/pages.blade.php
+++ resources/views/components/pages.blade.php
@@ -1,8 +1,10 @@
 <ul class="flex flex-col w-full {{ $classes }}">
     @foreach ($pages as $page)
-        <li class="cursor-pointer hover:text-blue rounded-sm border-gray-200 border-l-2 border-r-2 border-t-2 p-3 last:border-b-2">
-            <i class="fa-solid fa-earth-europe text-blue mr-1"></i>
-            <a href="{{ route('site.page', ['page' => $page->slug]) }}">{{ $page->title }}</a>
-        </li>
+        <a class="border-l-2 border-r-2 border-t-2 last:border-b-2 rounded-sm border-gray-200 cursor-pointer hover:text-blue p-3" href="{{ route('site.page', ['page' => $page->slug]) }}">
+            <li class="">
+                <i class="fa-solid fa-earth-europe text-blue mr-1"></i>
+                {{ $page->title }}
+            </li>
+        </a>
     @endforeach
 </ul>
